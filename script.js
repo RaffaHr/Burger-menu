88c460d03970108ec3debdb3683b626a7af3d10f
RaffaHr <128329385+RaffaHr@users.noreply.github.com>
2024-05-17 16:39:26 -0300

    feat: modificado horario de funcionamento.

--- script.js
+++ script.js
@@ -228,7 +228,7 @@ function removeItemCart(name) {
 function checkRestaurantOpen() {
   const data = new Date()
   const hora = data.getHours()
-  return hora >= 18 && hora <22
+  return hora >= 12 && hora <22
 }
 
 const spanItem = document.getElementById('date-span')
